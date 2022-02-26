9f699f7b0ad25d35507b8c8ddd688cb5082b4b2c
limon <limon222020@gmail.com>
2022-02-27 02:12:07 +0600

    todolist

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Todo List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+
